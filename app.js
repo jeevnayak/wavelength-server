b8e5bd59882c3660b7a348d3ef9a66cf365210f9
Rajeev Nayak <jeev.nayak@gmail.com>
2017-06-12 18:25:54 -0700

    hook up sentry

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var graphqlServerExpress = require("graphql-server-express");
 var graphqlTools = require("graphql-tools");
+var raven = require("raven");
 
 var loaders = require("./data/loaders");
 var models = require("./data/models");
@@ -10,6 +11,10 @@ var schema = require("./data/schema");
 
 var app = express();
 app.set("port", process.env.PORT || 5000);
+
+raven.config("https://2708fb4da3074c499c891cd4a50040c9:4f69d03fd5ea47158b367a74fdafeb39@sentry.io/178891").install();
+app.use(raven.requestHandler());
+app.use(raven.errorHandler());
 
 var executableSchema = graphqlTools.makeExecutableSchema({
   typeDefs: schema,
